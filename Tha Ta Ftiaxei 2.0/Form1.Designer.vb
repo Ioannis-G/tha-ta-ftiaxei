ef3aca17344079e426adc9bf9a9b285a1346379b
Ioannis Gaitaneris <113134133+Ioannis-G@users.noreply.github.com>
2024-06-28 02:11:20 +0300

    Initial Commit

---
+++ Tha Ta Ftiaxei 2.0/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim Ellada_2_0 As PictureBox
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Ellada_2_0 = New PictureBox()
+        CType(Ellada_2_0, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Ellada_2_0
+        ' 
+        Ellada_2_0.Image = CType(resources.GetObject("Ellada_2_0.Image"), Image)
+        Ellada_2_0.Location = New Point(578, 370)
+        Ellada_2_0.Name = "Ellada_2_0"
+        Ellada_2_0.Size = New Size(228, 76)
+        Ellada_2_0.SizeMode = PictureBoxSizeMode.StretchImage
+        Ellada_2_0.TabIndex = 7
+        Ellada_2_0.TabStop = False
+        Ellada_2_0.WaitOnLoad = True
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(121, 395)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(114, 41)
+        Button1.TabIndex = 0
+        Button1.Text = "Open GeoJSON File"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(364, 395)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(100, 41)
+        Button2.TabIndex = 1
+        Button2.Text = "Copy to Clipboard"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(12, 12)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.ScrollBars = ScrollBars.Vertical
+        TextBox1.Size = New Size(560, 331)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.Blue
+        Label1.Location = New Point(614, 12)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(179, 29)
+        Label1.TabIndex = 3
+        Label1.Text = "Tha Ta Ftiaxei "
+        Label1.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(601, 62)
+        Label2.MaximumSize = New Size(200, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(192, 136)
+        Label2.TabIndex = 4
+        Label2.Text = "This program allows you to process GeoJSON files, converting coordinates to DMS format and handling various geometry types, so as to assist with TopSky and GroundRadar map development. "
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(597, 229)
+        Label3.MaximumSize = New Size(200, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(196, 51)
+        Label3.TabIndex = 5
+        Label3.Text = "To get started, click on 'Open File' and Select a supported GeoJSON file."
+        Label3.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Arial", 9.75F, FontStyle.Italic, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.Gray
+        Label4.Location = New Point(614, 311)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(158, 32)
+        Label4.TabIndex = 6
+        Label4.Text = "Author: Ioannis Gaitaneris" & vbCrLf & "License: MIT"
+        Label4.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(6F, 13F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(851, 470)
+        Controls.Add(Ellada_2_0)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Tha Ta Ftiaxei "
+        CType(Ellada_2_0, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Ellada_2_0 As PictureBox
+
+End Class
---
+++ Tha Ta Ftiaxei 2.0/Form1.vb
@@ -0,0 +1,183 @@
+﻿Imports System
+Imports System.Windows.Forms
+Imports System.IO
+Imports Newtonsoft.Json.Linq
+Imports System.Globalization
+Public Class Form1
+    'Open GeoJSON File Button Logic
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim openFileDialog As OpenFileDialog = New OpenFileDialog With {
+            .Filter = "GeoJSON files (*.geojson)|*.geojson|All files (*.*)|*.*",
+            .RestoreDirectory = True
+        }
+
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+            TextBox1.Clear()
+
+            Dim filepath As String = openFileDialog.FileName
+            ProcessGeoJSONFile(filepath)
+        End If
+    End Sub
+
+    'Convert to DMS Function
+    Private Function ConverttoDMS(lat As Double, lon As Double) As String
+        Dim latHemisphere As String = If(lat >= 0, "N", "S")
+        Dim lonHemisphere As String = If(lon >= 0, "E", "W")
+
+        lat = Math.Abs(lat)
+        lon = Math.Abs(lon)
+
+        Dim latDeg As Integer = Math.Floor(lat)
+        Dim lonDeg As Integer = Math.Floor(lon)
+
+        Dim latMin As Integer = Math.Floor((lat - latDeg) * 60)
+        Dim lonMin As Integer = Math.Floor((lon - lonDeg) * 60)
+
+        Dim latSec As Double = ((lat - latDeg - latMin / 60) * 3600)
+        Dim lonSec As Double = ((lon - lonDeg - lonMin / 60) * 3600)
+
+        Return String.Format("{0}{1:00}.{2:00}.{3:00.000}:{4}{5:00}.{6:00}.{7:00.000}", latHemisphere, latDeg, latMin, latSec, lonHemisphere, lonDeg, lonMin, lonSec)
+    End Function
+
+    'Process GeoJSON File Method
+    Private Sub ProcessGeoJSONFile(filePath As String)
+        Dim jsonContent As String = File.ReadAllText(filePath)
+        Dim geoJson As JObject = JObject.Parse(jsonContent)
+
+        Dim features As JToken = geoJson("features")
+        Dim containsPolygonOrMultiPolygon As Boolean = features.Any(Function(feature) feature("geometry")("type").ToString() = "Polygon" OrElse feature("geometry")("type").ToString() = "MultiPolygon")
+
+        Dim polygonColor As String = String.Empty
+        If containsPolygonOrMultiPolygon Then
+            polygonColor = InputBox("Enter the color for the polygons:", "Polygon Color", "yourcolorhere")
+            If String.IsNullOrWhiteSpace(polygonColor) Then
+                MessageBox.Show("No color was entered. Processing will continue without color for polygons.", "Missing Color", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End If
+
+        Dim supportedTypeFound As Boolean = False
+        Dim isFirstPolygon As Boolean = True
+
+        For Each feature In features
+            Dim geometry As JToken = feature("geometry")
+            Dim type As String = geometry("type").ToString()
+            Dim coordinates As JToken = geometry("coordinates")
+
+            Select Case type
+                Case "Polygon", "MultiPolygon"
+                    supportedTypeFound = True
+                    If Not String.IsNullOrWhiteSpace(polygonColor) Then
+                        If Not isFirstPolygon Then
+                            TextBox1.AppendText("COLOR:" & polygonColor & Environment.NewLine)
+                        End If
+                        ProcessPolygon(coordinates)
+                        isFirstPolygon = False
+                    End If
+
+                Case "LineString"
+                    supportedTypeFound = True
+                    ProcessLineString(coordinates)
+
+                Case "MultiLineString"
+                    supportedTypeFound = True
+                    ProcessLineString(coordinates, True)
+
+                Case "Point", "MultiPoint"
+                    supportedTypeFound = True
+                    ProcessPoint(coordinates)
+
+                Case Else
+                    ' Unsupported type, do nothing
+            End Select
+        Next
+
+        If Not supportedTypeFound Then
+            MessageBox.Show("The selected file does not contain any of the supported geometry types. Please select a valid file.", "Invalid file", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Button1_Click(Nothing, Nothing) 'Reopen file dialog
+        End If
+    End Sub
+
+    'Copy to Clipboard Button Logic
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Clipboard.SetText(TextBox1.Text)
+    End Sub
+
+    'Polygon Processing Method
+    Private Sub ProcessPolygon(polygon As JToken)
+        For Each ring As JToken In polygon
+            For Each coord As JToken In ring.First
+                Dim lon As Double = coord(0).ToObject(Of Double)()
+                Dim lat As Double = coord(1).ToObject(Of Double)()
+                Dim formattedCoord As String = "COORD:" & ConverttoDMS(lat, lon)
+                TextBox1.AppendText(formattedCoord & Environment.NewLine)
+            Next
+        Next
+    End Sub
+
+    'LineString Processing Method
+    Private Sub ProcessLineString(lineCoordinates As JToken, Optional isMultiLineString As Boolean = False)
+        If isMultiLineString Then
+            For Each line As JToken In lineCoordinates
+                ProcessSingleLineString(line)
+            Next
+        Else
+            ProcessSingleLineString(lineCoordinates)
+        End If
+    End Sub
+
+    'LineString Processing Helper Method
+    Private Sub ProcessSingleLineString(lineCoordinates As JToken)
+        For i As Integer = 0 To lineCoordinates.Count - 2
+            Dim startCoord As JToken = lineCoordinates(i)
+            Dim endCoord As JToken = lineCoordinates(i + 1)
+
+            Dim startLon As Double = startCoord(0).ToObject(Of Double)()
+            Dim startLat As Double = startCoord(1).ToObject(Of Double)()
+            Dim endLon As Double = endCoord(0).ToObject(Of Double)()
+            Dim endLat As Double = endCoord(1).ToObject(Of Double)()
+
+            Dim formattedStartCoord As String = ConverttoDMS(startLat, startLon)
+            Dim formattedEndCoord As String = ConverttoDMS(endLat, endLon)
+
+            TextBox1.AppendText($"LINE:{formattedStartCoord}:{formattedEndCoord}" & Environment.NewLine)
+        Next
+    End Sub
+
+    'Point Processing Method
+    Private Sub ProcessPoint(point As JToken, Optional isMultiPoint As Boolean = False)
+        If isMultiPoint Then
+            For Each pt As JToken In point.Children()
+                ProcessSinglePoint(pt)
+            Next
+        Else
+            ProcessSinglePoint(point)
+        End If
+    End Sub
+
+    'Point Processing Helper Method
+    Private Sub ProcessSinglePoint(point As JToken)
+        If point.HasValues AndAlso point(0).Type = JTokenType.Array Then
+            ' Assuming the first element of the array is another array with at least two elements (longtitude and latitude).
+            Dim coordinateArray As JArray = point(0)
+            If coordinateArray.Count >= 2 Then
+                Dim lon As Double = coordinateArray(0).ToObject(Of Double)()
+                Dim lat As Double = coordinateArray(1).ToObject(Of Double)()
+                Dim formattedCoord As String = ConverttoDMS(lat, lon)
+
+                Dim label As String = String.Empty
+                While String.IsNullOrWhiteSpace(label)
+                    label = InputBox($"Enter the label {formattedCoord}", "Point Label", "yourlabelhere")
+                    If String.IsNullOrWhiteSpace(label) Then
+                        MessageBox.Show("No label was entered. Please try again.", "Label Required", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                End While
+
+                TextBox1.AppendText($"TEXT:{formattedCoord}:{label}" & Environment.NewLine)
+            End If
+        Else
+            Throw New InvalidOperationException("No coordinates found or unexpected structure")
+        End If
+
+    End Sub
+
+End Class
